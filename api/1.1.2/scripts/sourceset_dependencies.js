d10c705076203fd6de356ecccde318a1f2eae5e8
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2023-07-24 15:34:37 +0800

    update

--- api/1.1.2/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies='{":router:dokkaJavadoccccc/androidTestRelease":[],":router:dokkaJavadoccccc/debug":[],":router:dokkaJavadoccccc/main":[],":router:dokkaJavadoccccc/release":[]}'
